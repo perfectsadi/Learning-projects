ea9955a7dba0f83656ce173768346ec24a5110e2
Sadi <sadiytofficial@gmail.com>
2026-05-28 01:14:08 +0600

    ok

---
+++ Python/definedfunction.py
@@ -0,0 +1,9 @@
+def hello(name):
+    print(f"Hello, {name}!")
+# a function is a block of code that performs a specific task.
+
+hello("Alice") # Calling the function to execute its code.
+
+
+#again
+hello("Bob")
